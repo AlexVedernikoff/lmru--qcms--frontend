87d6aa8db2d00f9907292ddaac645f95b9c6c430
Aleftina Russkina <alka-wolfik@yandex.ru>
2023-05-26 10:33:21 +0300

    feat: card ToDo

--- src/components/Common/CardTodo/index.tsx
+++ src/components/Common/CardTodo/index.tsx
@@ -2,9 +2,68 @@ import styles from './styles.module.css';
 import AddTodoIcon from '../../Icons/AddTodoIcon';
 import {useTranslation} from 'react-i18next';
 import FlagIcon from '../../Icons/FlagIcon';
+import { IconComponent } from '@fronton/icons-react';
+
+interface IItem {
+    label: string;
+    value: number;
+    valueImportant: number;
+    icon: IconComponent | React.FC;
+}
 
 export const CardTodo = () => {
-    const {t} = useTranslation('cardTodo');
+    const {t} = useTranslation('dashboard');
+
+    const items: IItem[] = [
+        {
+            icon: FlagIcon,
+            label: t('List.ReChecking'),
+            valueImportant: 2,
+            value: 4,
+        },
+        {
+            icon: FlagIcon,
+            label: t('List.QualifyingTasksLaunch'),
+            valueImportant: 2,
+            value: 4,
+        },
+        {
+            icon: FlagIcon,
+            label: t('List.TransferDocuments'),
+            valueImportant: 2,
+            value: 4,
+        },
+        {
+            icon: FlagIcon,
+            label: t('List.ProductsWithErrors'),
+            valueImportant: 2,
+            value: 4,
+        },
+        {
+            icon: FlagIcon,
+            label: t('List.ProductsWithoutQualityModels'),
+            valueImportant: 2,
+            value: 4,
+        },
+        {
+            icon: FlagIcon,
+            label: t('List.CertificationTasksLaunch'),
+            valueImportant: 2,
+            value: 4,
+        },
+        {
+            icon: FlagIcon,
+            label: t('List.AdditionalQualificationProducts'),
+            valueImportant: 2,
+            value: 4,
+        },
+        {
+            icon: FlagIcon,
+            label: t('List.TasksAwaitingValidation'),
+            valueImportant: 2,
+            value: 4,
+        },
+    ];
 
     return (
         <div className={styles.container}>
@@ -16,86 +75,18 @@ export const CardTodo = () => {
             </div>
             <div>
                 <ul className={styles.list}>
-                    <li>
-                        <div>{t('List.ReChecking')}</div>
-                        <div className={styles.containerValues}>
-                            <div>
-                                <FlagIcon />
-                            </div>
-                            <div className={styles.importantValue}>2</div>
-                            <div>4</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div>{t('List.QualifyingTasksLaunch')}</div>
-                        <div className={styles.containerValues}>
-                            <div>
-                                <FlagIcon />
-                            </div>
-                            <div className={styles.importantValue}>1</div>
-                            <div>4</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div>{t('List.TransferDocuments')}</div>
-                        <div className={styles.containerValues}>
-                            <div>
-                                <FlagIcon />
-                            </div>
-                            <div className={styles.importantValue}>9</div>
-                            <div>4</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div>{t('List.ProductsWithErrors')}</div>
-                        <div className={styles.containerValues}>
-                            <div>
-                                <FlagIcon />
-                            </div>
-                            <div className={styles.importantValue}>1</div>
-                            <div>4</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div>{t('List.ProductsWithoutQualityModels')}</div>
-                        <div className={styles.containerValues}>
-                            <div>
-                                <FlagIcon />
-                            </div>
-                            <div className={styles.importantValue}>2</div>
-                            <div>4</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div>{t('List.CertificationTasksLaunch')}</div>
-                        <div className={styles.containerValues}>
-                            <div>
-                                <FlagIcon />
-                            </div>
-                            <div className={styles.importantValue}>2</div>
-                            <div>4</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div>{t('List.AdditionalQualificationProducts')}</div>
-                        <div className={styles.containerValues}>
-                            <div>
-                                <FlagIcon />
-                            </div>
-                            <div className={styles.importantValue}>2</div>
-                            <div>4</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div>{t('List.TasksAwaitingValidation')}</div>
-                        <div className={styles.containerValues}>
-                            <div>
-                                <FlagIcon />
-                            </div>
-                            <div className={styles.importantValue}>2</div>
-                            <div>4</div>
-                        </div>
-                    </li>
+                    {items.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <div>{item.label}</div>
+                                <div className={styles.containerValues}>
+                                    <div><item.icon/></div>
+                                    <div className={styles.importantValue}>{item.valueImportant}</div>
+                                    <div>{item.value}</div>
+                                </div>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         </div>
--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -5,7 +5,7 @@ import models_ru from './ru/models.json';
 import providers_ru from './ru/providers.json';
 import tasks_ru from './ru/tasks.json';
 import sidebar_ru from './ru/sidebar.json';
-import cardTodo_ru from './ru/cardTodo.json';
+import dashboard_ru from './ru/dashboard.json';
 
 const resources = {
     en: {
@@ -14,7 +14,7 @@ const resources = {
         providers: providers_ru,
         tasks: tasks_ru,
         sidebar: sidebar_ru,
-        cardTodo: cardTodo_ru
+        dashboard: dashboard_ru
     },
     ru: {
         products: products_ru,
@@ -22,7 +22,7 @@ const resources = {
         providers: providers_ru,
         tasks: tasks_ru,
         sidebar: sidebar_ru,
-        cardTodo:  cardTodo_ru
+        dashboard:  dashboard_ru
     },
 };
 
