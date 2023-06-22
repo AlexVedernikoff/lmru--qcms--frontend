6bd816e4d0004bb13397df4fa35330760bcdbe19
AlexVedernikoff <templar_sc@mail.ru>
2023-06-22 14:37:46 +0300

    даты правим

--- src/components/Products/WithDocuments/DocumentsFilter/prepareBody.ts
+++ src/components/Products/WithDocuments/DocumentsFilter/prepareBody.ts
@@ -10,7 +10,6 @@ const initialBody = {
 
 export const prepareBody = (productsDocumentsFiltersState: IFilters) => {
     const requsetBody: any = {...initialBody};
-    const {searchBy} = requsetBody;
 
     const {productNumberKey, productNumberValue, supplierNameKey, supplierNameValue, dates} =
         productsDocumentsFiltersState;
@@ -21,7 +20,11 @@ export const prepareBody = (productsDocumentsFiltersState: IFilters) => {
     requsetBody.searchBy = {
         [productNumberKey]: productNumberValue,
         [supplierNameKey]: supplierNameValue,
-        [String(dateType)]:
+        ...productsDocumentsFiltersState,
+    };
+
+    if (dateType) {
+        requsetBody.searchBy[String(dateType)] =
             startDate && endDate
                 ? {
                       'Start date for search (After)': newDateArr.includes(String(dateType))
@@ -31,9 +34,9 @@ export const prepareBody = (productsDocumentsFiltersState: IFilters) => {
                           ? new Date(endDate)
                           : endDate,
                   }
-                : undefined,
-        ...productsDocumentsFiltersState,
-    };
+                : undefined;
+    }
+
     delete requsetBody.searchBy.productNumberKey;
     delete requsetBody.searchBy.productNumberValue;
     delete requsetBody.searchBy.supplierNameKey;
@@ -42,31 +45,3 @@ export const prepareBody = (productsDocumentsFiltersState: IFilters) => {
 
     return requsetBody;
 };
-
-const searchByJSON = {
-    type: ['string'], // documentType в предыдущей документации
-    regulatoryStatus: ['string'],
-    // *******************
-    productDescription: 'string',
-    productCode: 'string',
-    productTNVEDcode: 'string',
-    ean: 'string',
-    // *******************
-    supplierRMSCode: 'string',
-    supplierName: 'string',
-    supplierTaxIdentifer: 'string',
-    supplierBusinessLicense: 'string', // optional
-    // *******************
-    productManagementNomenclatureDepartmentId: [0],
-    productManagementNomenclatureSubdepartmentId: [0],
-    productManagementNomenclatureTypeId: [0],
-    productManagementNomenclatureSubtypeId: [0],
-    // *******************
-    productModelNomenclatureDepartmentId: ['string'],
-    productModelNomenclatureSubdepartmentId: ['string'],
-    productModelNomenclatureConsolidationId: ['string'],
-    productModelNomenclatureCodeId: ['string'],
-};
-
-// approvedBy исчезло.  !скрыть
-// qe скрыть.
