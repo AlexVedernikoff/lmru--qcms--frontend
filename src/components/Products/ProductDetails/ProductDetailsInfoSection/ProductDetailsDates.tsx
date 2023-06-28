3f4a0239e95d71ba556ce849e51e2710d8c86429
nikita gavrilov <nikita.gavrilov@nordclan.com>
2023-06-29 02:18:23 +0300

    update: date format

--- src/components/Products/ProductDetails/ProductDetailsInfoSection/ProductDetailsDates.tsx
+++ src/components/Products/ProductDetails/ProductDetailsInfoSection/ProductDetailsDates.tsx
@@ -4,13 +4,15 @@ import styles from '../../../Common.module.css';
 import {useGetDetailsForProductsQuery} from '../productDetailsApi';
 
 import {productId, securityCode} from '../mockProductDetails';
+import {converStringToDateTime} from '../../../../utils/convertDateFromServer';
 
 const ProductDetailsDates: React.FC = () => {
     const {t} = useTranslation('products');
 
     const {data: details} = useGetDetailsForProductsQuery({productId, securityCode});
 
-    const createdAt = details?.creationInformation.createdAt;
+    const createdAt =
+        details?.creationInformation.createdAt && converStringToDateTime(details?.creationInformation.createdAt);
 
     return (
         <Grid className={styles.sectionItem} rowGap={16} columnGap={16} columns="1fr" rows="36px">
