2ea33b6e469e76c3a6ce4a911ac4480b3aab7c63
leonid.meshakin <>
2023-07-02 23:05:05 +0300

    QCMS-147 update app layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
-import {Empty} from 'antd';
-import {Grid, GridItem} from 'fronton-react';
+import {useState} from 'react';
 import {Routes, Route, Navigate} from 'react-router-dom';
+import {Empty} from 'antd';
 import {APP_ROUTES, MODELS_ROUTES, PRODUCTS_ROUTES, PROVIDER_ROUTES, TASKS_ROUTES} from '../common/consts';
 import Header from './Layout/Header';
 import Sidebar from './Layout/Sidebar';
@@ -17,49 +17,55 @@ import Models from './Models';
 import Tasks from './Tasks';
 import Dashboard from './Dashboard';
 import TaskDetails from './Tasks/TaskDetails';
+import styles from './App.module.css';
 
-const App: React.FC = () => (
-    <Grid areas={['header header header', 'aside main spacer']} columnGap={16} columns="auto 1fr 12px">
-        <GridItem area="header">
-            <Header />
-        </GridItem>
+const App: React.FC = () => {
+    const [isMinified, setIsMinified] = useState(false);
 
-        <GridItem area="aside">
-            <Sidebar />
-        </GridItem>
+    const handleSidebarToggle = (isMinified: boolean) => {
+        setIsMinified(isMinified);
+    };
 
-        <GridItem area="main">
-            <br />
-            <Routes>
-                <Route path={APP_ROUTES.dashboard} element={<Dashboard />} />
-                <Route path={APP_ROUTES.providers} element={<Providers />} />
-                <Route path={PROVIDER_ROUTES.details} element={<ProviderDetails />} />
-                <Route path={APP_ROUTES.products} element={<Products />}>
-                    <Route path={PRODUCTS_ROUTES.withModels} element={<ProductsWithQualityModel />} />
-                    <Route path={PRODUCTS_ROUTES.withoutModels} element={<ProductsWithoutQualityModel />} />
-                    <Route path={PRODUCTS_ROUTES.transfer} element={<Transfer />} />
-                    <Route path={PRODUCTS_ROUTES.documents} element={<ProductsWithDocuments />} />
+    return (
+        <div className={styles.app}>
+            <div className={styles.header}>
+                <Header />
+            </div>
 
-                    <Route path={PRODUCTS_ROUTES.details} element={<ProductDetails />} />
+            <div className={styles.main}>
+                <div className={isMinified ? styles.sidebarMini : styles.sidebar}>
+                    <Sidebar isMinified={isMinified} onToggle={handleSidebarToggle} />
+                </div>
 
-                    <Route path={APP_ROUTES.products} element={<Navigate to={PRODUCTS_ROUTES.withModels} />} />
-                </Route>
-                <Route path={APP_ROUTES.tasks} element={<Tasks />}>
-                    <Route path={TASKS_ROUTES.details} element={<TaskDetails />} />
-                </Route>
-                <Route path={APP_ROUTES.models} element={<Models />}>
-                    <Route path={MODELS_ROUTES.details} element={<ModelDetails />} />
-                </Route>
-                <Route path={APP_ROUTES.settings} element={<Empty />} />
+                <div className={isMinified ? styles.containerFull : styles.container}>
+                    <Routes>
+                        <Route path={APP_ROUTES.dashboard} element={<Dashboard />} />
+                        <Route path={APP_ROUTES.providers} element={<Providers />} />
+                        <Route path={PROVIDER_ROUTES.details} element={<ProviderDetails />} />
+                        <Route path={APP_ROUTES.products} element={<Products />}>
+                            <Route path={PRODUCTS_ROUTES.withModels} element={<ProductsWithQualityModel />} />
+                            <Route path={PRODUCTS_ROUTES.withoutModels} element={<ProductsWithoutQualityModel />} />
+                            <Route path={PRODUCTS_ROUTES.transfer} element={<Transfer />} />
+                            <Route path={PRODUCTS_ROUTES.documents} element={<ProductsWithDocuments />} />
 
-                <Route path="*" element={<Navigate to={APP_ROUTES.dashboard} />} />
-            </Routes>
-        </GridItem>
+                            <Route path={PRODUCTS_ROUTES.details} element={<ProductDetails />} />
 
-        <GridItem area="spacer">
-            <span />
-        </GridItem>
-    </Grid>
-);
+                            <Route path={APP_ROUTES.products} element={<Navigate to={PRODUCTS_ROUTES.withModels} />} />
+                        </Route>
+                        <Route path={APP_ROUTES.tasks} element={<Tasks />}>
+                            <Route path={TASKS_ROUTES.details} element={<TaskDetails />} />
+                        </Route>
+                        <Route path={APP_ROUTES.models} element={<Models />}>
+                            <Route path={MODELS_ROUTES.details} element={<ModelDetails />} />
+                        </Route>
+                        <Route path={APP_ROUTES.settings} element={<Empty />} />
+
+                        <Route path="*" element={<Navigate to={APP_ROUTES.dashboard} />} />
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
