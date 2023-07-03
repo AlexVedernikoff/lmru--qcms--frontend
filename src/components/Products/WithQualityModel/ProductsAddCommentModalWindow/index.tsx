ce950671965220f587878fe64f4338874343cd09
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-07-03 13:39:54 +0300

    QCMS-340 (#93)
    Co-authored-by: Grevtsov Matvey <matvey.grevtsov@ax-team.corp>

--- src/components/Products/WithQualityModel/ProductsAddCommentModalWindow/index.tsx
+++ src/components/Products/WithQualityModel/ProductsAddCommentModalWindow/index.tsx
@@ -1,6 +1,7 @@
-import {Modal, ModalContent, ModalFooter, ModalHeader} from 'fronton-react';
+import {Grid, Modal, ModalContent, ModalFooter, ModalHeader, RegularButton, Textarea} from 'fronton-react';
 import {useTranslation} from 'react-i18next';
 import {IProduct} from '../../../../common/types/products';
+import {ChangeEvent, useState} from 'react';
 
 interface Props {
     show: boolean;
@@ -12,11 +13,40 @@ interface Props {
 const ProductsAddCommentModalWindow: React.FC<Props> = ({show, onClose, products}) => {
     const {t} = useTranslation('products');
 
+    const [text, setText] = useState<string>('');
+
+    const handleTextAreaChange = (event: ChangeEvent<HTMLTextAreaElement>, value: string) => {
+        setText(value);
+    };
+
+    const clear = () => setText('');
+
+    const handleClose = () => {
+        onClose();
+        clear();
+    };
+
+    const handleSubmit = () => {};
+
     return (
-        <Modal onClose={onClose} show={show}>
+        <Modal onClose={handleClose} show={show}>
             <ModalHeader title={t('WithModels.Actions.actions.addComment')} />
-            <ModalContent></ModalContent>
-            <ModalFooter></ModalFooter>
+            <ModalContent>
+                <Textarea
+                    onChange={handleTextAreaChange}
+                    placeholder={t('WithModels.addCommentModalWindow.textAreaPlaceholder')}
+                />
+            </ModalContent>
+            <ModalFooter>
+                <Grid justifyContent="right" columns="auto auto" columnGap="24px">
+                    <RegularButton variant="outline" size="l" onClick={handleClose}>
+                        {t('WithModels.addCommentModalWindow.closeModalButton')}
+                    </RegularButton>
+                    <RegularButton variant="primary" size="l" disabled={!text} onClick={handleSubmit}>
+                        {t('WithModels.addCommentModalWindow.submitButton')}
+                    </RegularButton>
+                </Grid>
+            </ModalFooter>
         </Modal>
     );
 };
