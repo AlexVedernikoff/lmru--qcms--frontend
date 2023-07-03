debe4fb64c3fb0430548a1b1675ab06886324fd6
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-07-03 11:46:50 +0300

    QCMS-336 (#89)
    * QCMS-336
    
    * QCMS-336
    
    * QCMS-308
    
    * QCMS-308
    
    * QCMS-308
    
    ---------
    
    Co-authored-by: Grevtsov Matvey <matvey.grevtsov@ax-team.corp>

--- src/components/Products/WithQualityModel/ProductsAddDocumentModalWindow/index.tsx
+++ src/components/Products/WithQualityModel/ProductsAddDocumentModalWindow/index.tsx
@@ -1,6 +1,13 @@
 import {useTranslation} from 'react-i18next';
-import {Modal, ModalContent, ModalFooter, ModalHeader} from 'fronton-react';
+import {DatePicker, Grid, Modal, ModalContent, ModalFooter, ModalHeader, RegularButton} from 'fronton-react';
 import {IProduct} from '../../../../common/types/products';
+import {useState, useMemo, useEffect} from 'react';
+import {IDocumentMetaData} from '../../../../common/types/files';
+import FileUploadForm from '../../../Common/FileUploadForm';
+import {CustomSwitch} from '../../../Common/Switch/CustomSwitch';
+import tasksApi from '../../../Tasks/tasksApi';
+
+import styles from './styles.module.css';
 
 interface Props {
     show: boolean;
@@ -9,14 +16,144 @@ interface Props {
     onSubmit?: () => void;
 }
 
+interface FormState {
+    isPartial: boolean;
+    startDate?: string;
+    endDate?: string;
+    file?: File;
+}
+
+const initialFormState: FormState = {
+    isPartial: false,
+};
+
 const ProductsAddDocumentModalWindow: React.FC<Props> = ({show, onClose, products}) => {
     const {t} = useTranslation('products');
+    const [formState, setFormState] = useState<FormState>(initialFormState);
+
+    const [createDocument, createDocumentResult] = tasksApi.endpoints.createDocument.useMutation();
+
+    const isButtonDisabled = useMemo(
+        () =>
+            createDocumentResult.isLoading ||
+            !formState.file ||
+            (formState.isPartial ? !formState.startDate : !formState.startDate || !formState.endDate),
+        [createDocumentResult.isLoading, formState]
+    );
+
+    const handleClose = () => {
+        if (createDocumentResult.isLoading) return;
+        onClose();
+        setFormState(initialFormState);
+    };
+
+    useEffect(() => {
+        if (!formState.file) return;
+
+        if (createDocumentResult.isSuccess) {
+            handleClose();
+        }
+
+        if (createDocumentResult.isError) {
+            type TDataError = {data: {errors: [{message: string}]}};
+            alert((createDocumentResult.error as unknown as TDataError)?.data?.errors?.[0]?.message);
+        }
+    }, [createDocumentResult.error, createDocumentResult.isError, createDocumentResult.isSuccess, onClose]);
+
+    const handleStartDateChange = (date: string[]) => {
+        setFormState(prevState => ({...prevState, startDate: date[0]}));
+    };
+
+    const handleEndDateChange = (date: string[]) => {
+        setFormState(prevState => ({...prevState, endDate: date[0]}));
+    };
+
+    const handleSwitch: React.ChangeEventHandler<HTMLInputElement> = ({target}) => {
+        setFormState(prevState => ({...prevState, isPartial: target.checked}));
+        if (target.checked) {
+            setFormState(prevState => ({...prevState, endDate: undefined}));
+        }
+    };
+
+    const handleFileSelect = (file: File | undefined) => {
+        setFormState(prevState => ({...prevState, file}));
+    };
+
+    const handleSubmit = () => {
+        if (!formState.file) return;
+
+        const documentMetaData: IDocumentMetaData = {
+            createdBy: 'currentUser',
+            type: 'document',
+            isForLot: formState.isPartial,
+            isTemplate: false,
+            fileName: formState.file.name,
+            issueDate: formState.startDate,
+            expireDate: formState.endDate,
+            productsDetails: [],
+        };
+
+        const body = new FormData();
+        body.append('file1', formState.file);
+        body.append('documentMetaData', JSON.stringify(documentMetaData));
+
+        createDocument({
+            header: {
+                securityCode: 'security_code',
+            },
+            body,
+        });
+    };
 
     return (
-        <Modal onClose={onClose} show={show}>
+        <Modal show={show} onClose={handleClose} size="m">
             <ModalHeader title={t('WithModels.Actions.actions.addDocument')} />
-            <ModalContent></ModalContent>
-            <ModalFooter></ModalFooter>
+            <ModalContent className={styles.content}>
+                <Grid gap={24} className={styles.grid}>
+                    <FileUploadForm onFileSelect={handleFileSelect} />
+
+                    <CustomSwitch checked={formState.isPartial} handleChange={handleSwitch} name="Партийный" />
+
+                    <Grid gap={36} columns="auto auto">
+                        <DatePicker
+                            date={[formState.startDate!]}
+                            onChange={handleStartDateChange}
+                            label={'Дата начала действия'}
+                            size="s"
+                            view="single"
+                            mode="single"
+                            className={styles.dateInput}
+                        />
+
+                        {!formState.isPartial && (
+                            <DatePicker
+                                date={[formState.endDate!]}
+                                onChange={handleEndDateChange}
+                                label={'Дата окончания'}
+                                size="s"
+                                view="single"
+                                mode="single"
+                                className={styles.dateInput}
+                            />
+                        )}
+                    </Grid>
+
+                    <br />
+                    <br />
+                    <br />
+                </Grid>
+            </ModalContent>
+            <ModalFooter>
+                <Grid columnGap={16} columns="repeat(2, 1fr)">
+                    <RegularButton onClick={onClose} size="m" variant="outline" disabled={isButtonDisabled}>
+                        {t('WithModels.addDocumentModalWindow.closeModalButton')}
+                    </RegularButton>
+
+                    <RegularButton onClick={handleSubmit} disabled={isButtonDisabled}>
+                        {t('WithModels.addDocumentModalWindow.submitButton')}
+                    </RegularButton>
+                </Grid>
+            </ModalFooter>
         </Modal>
     );
 };
