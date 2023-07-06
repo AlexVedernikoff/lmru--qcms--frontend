12949da2672e7ab1ffee1a74c01531f70daaebae
leonid.meshakin <>
2023-07-06 10:40:40 +0300

    quick fix

--- src/utils/convertDateFromServer.ts
+++ src/utils/convertDateFromServer.ts
@@ -1,11 +1,19 @@
 import format from 'date-fns/format';
 
 export const convertDateFromServer = (date: string) => {
-    const res = format(new Date(date), 'dd.MM.yyyy');
-    return res;
+    try {
+        const res = format(new Date(date), 'dd.MM.yyyy');
+        return res;
+    } catch (err) {
+        return '';
+    }
 };
 
 export const converStringToDateTime = (date: string | Date) => {
-    const res = format(new Date(date), 'yyyy.MM.dd HH:mm:ss');
-    return res;
+    try {
+        const res = format(new Date(date), 'yyyy.MM.dd HH:mm:ss');
+        return res;
+    } catch (err) {
+        return '';
+    }
 };
