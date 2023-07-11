b551ad05aae203b5870f9ca526d3c345b4394445
AlexVedernikoff <templar_sc@mail.ru>
2023-07-11 15:16:22 +0300

    feature/QCMS-362 убираем лишние пропсы возвращаем иконку загрузки

--- src/components/Providers/ProvidersList/index.tsx
+++ src/components/Providers/ProvidersList/index.tsx
@@ -2,26 +2,12 @@ import {Grid} from 'fronton-react';
 import ProvidersFilter from './Filter';
 import ProvidersTable from './Table';
 import styles from '../../Common.module.css';
-import {useGetProviders} from './hooks/useGetProviders';
-import {useGetManagementNomenclatureQuery, useGetProductModelNomenclatureQuery} from '../services';
 
 const ProvidersList: React.FC = () => {
-    const {loadProvidersList} = useGetProviders({
-        pageIndex: 1,
-        pageSize: 1,
-    });
-
-    const {data: modelNomenclature} = useGetProductModelNomenclatureQuery();
-    const {data: managementNomenclature} = useGetManagementNomenclatureQuery();
-
     return (
         <Grid rowGap={16}>
             <Grid rowGap={16}>
-                <ProvidersFilter
-                    modelNomenclature={modelNomenclature}
-                    managementNomenclature={managementNomenclature}
-                    loadProvidersList={loadProvidersList}
-                />
+                <ProvidersFilter />
             </Grid>
 
             <Grid rowGap={16} className={styles.panel}>
